894f48250c92ebbc49088b4d926451659be17514
MadhuliPadwal <madhulishilpa1181@gmail.com>
2024-08-02 11:51:54 +0530

    Add files via upload

---
+++ practical.py
@@ -0,0 +1,23 @@
+
+list1=["Aditya","Raj",22]
+list2=["India",9,1.0]
+'''adding element to last'''
+list1.append("orange")
+print(list1)
+'''taking length of the list'''
+print(len(list1))
+'''inserting element to list 1'''
+list1.insert(0, 'Andry')
+print(list1)
+'''concatenating the 2 lists'''
+print(list1+list2)
+'''pop the element'''
+list1.pop(0)
+forces = ['Army', 'Navy', 'Airforce']
+print(forces)
+
+
+
+
+
+
